993e1eb1898593cd4f7654c869b7a1e1fc945815
Your NamePrashant <prsharma0403@gmail.com>
2025-08-23 08:33:37 +0530

    code push

--- src/component/Body.jsx
+++ src/component/Body.jsx
@@ -1,15 +1,40 @@
-import { Outlet } from "react-router-dom";
+import { Navigate, Outlet, useNavigate } from "react-router-dom";
 import NavBar from "./NavBar";
 import Footer from "./Footer";
-
-const Body=()=>{
-    return(
+import axios from "axios";
+import { useDispatch, useSelector } from "react-redux";
+import { addUser } from "../utils/userSlice";
+import { useEffect } from "react";
+import { BASE_URL } from "../utils/constant";
+const Body = () => {
+  const dispatch = useDispatch();
+  const Navigate=useNavigate()
+  const userData=useSelector((store)=>store.user);
+  const fetchUser = async () => {
+    if(userData) return
+    try {
+      const user = await axios.get(BASE_URL + "/profile/view", {
+        withCredentials: true,
+      });
+      console.log("M yuser"+user)
+      dispatch(addUser(resizeBy.data));
+    } catch (err) {
+      console.error(err);
+      Navigate("/login");
+    }
+  };
+  useEffect(() => {
+    
+ fetchUser();
+    
+   
+  }, []);
+  return (
     <>
-   
-   
-     <NavBar />
-      <Outlet/>
-    <Footer />
+      <NavBar />
+      <Outlet />
+      <Footer />
     </>
-    )}
+  );
+};
 export default Body;
